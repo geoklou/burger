5d1438917f9ca2fea6a584abba3aae5474c41fef
geoklou <g-lou2@northwestern.edu>
2017-08-15 18:50:40 -0500

    install and db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('double cheese hamburger');
+INSERT INTO burgers (burger_name) VALUES ('veggie burger');
+INSERT INTO burgers (burger_name) VALUES ('50 cal burger');
